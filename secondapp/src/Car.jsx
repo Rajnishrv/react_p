3b1caaae05d173c72d49867613cce2e9dae031aa
rajnish <rajnishkumar550443@gmail.com>
2024-09-26 09:30:15 +0530

    class bssed component

---
+++ secondapp/src/Car.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class Car extends React.Component{
+    render(){
+        return(
+            <div className="d1">
+                <h1>i am car component</h1>
+            </div>
+        )
+    }
+}
+export default Car;
---
+++ secondapp/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class Home extends React.Component{
+    render(){
+        return(
+            <div className="d1">
+                <h1>i am home component</h1>
+            </div>
+        )
+    }
+}
+export default Home;
