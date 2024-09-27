4ce9b6fd5982c4ebae7cb272644ef3a91cd8adad
rajnish <rajnishkumar550443@gmail.com>
2024-09-27 09:28:51 +0530

    class based component

--- secondapp/src/App.js
+++ secondapp/src/App.js
@@ -4,9 +4,17 @@ import Car from './Car';
 import Home from './Home';
 
 function App() {
+  function first(){
+    console.log("first Button clicked");
+  }
+    function second(sec){
+      console.log(sec," Button clicked");
+    }5
   return (
     <>
-      <h1>App component</h1>
+      <h1 style={{backgroundColor:"yellow", color:"red"}}>App component</h1>
+      <button onClick={first}>first</button>
+      <button onClick={()=>{second("second")}}>second</button>
       <Car />
       <Home />
     </>
--- secondapp/src/Car.jsx
+++ secondapp/src/Car.jsx
@@ -1,9 +1,11 @@
+import "./Car.css"
 import React from "react";
 class Car extends React.Component{
     render(){
         return(
             <div className="d1">
                 <h1>i am car component</h1>
+                <h2>jdljkojfodjljl</h2>
             </div>
         )
     }
